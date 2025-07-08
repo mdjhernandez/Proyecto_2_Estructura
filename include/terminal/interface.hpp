c8685f47e67716de4fe87ebc9fefd63e2551307f
Arturo Pérez <arjperez31@gmail.com>
2025-07-07 23:36:12 -0400

    feat: modificandoel header de terminal

--- include/terminal/interface.hpp
+++ include/terminal/interface.hpp
@@ -3,12 +3,4 @@
 
 void comandLS(NodeTree *node, int nivel);
 
-void comandCD(NodeTree *node, std::string path);
-
-void comandTouch(NodeTree *node, std::string path, std::string name);
-
-void comandMKDir(NodeTree *node, std::string path, std::string name);
-
-void comandRM(NodeTree *node, std::string path);
-
 void terminalInterface();
